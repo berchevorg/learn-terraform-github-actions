36e97c7b3ed2402e62c6b322b4a7cb6597c28304
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-10-27 16:16:44 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,30 +8,25 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "georgiman"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
   }
 }
 
 resource "random_pet" "name" {
-  length    = "3"
+  length    = "5"
   separator = "-"
 }
 
 resource "random_pet" "name2" {
-  length    = "3"
+  length    = "5"
   separator = "-"
 }
 
 resource "random_pet" "name3" {
-  length    = "3"
+  length    = "5"
   separator = "-"
 }
 
 resource "random_pet" "name4" {
-  length    = "3"
+  length    = "5"
   separator = "-"
 }
