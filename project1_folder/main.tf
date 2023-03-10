b7ed566abeff067b0f2ae1c140ee1b0446af2d34
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2023-03-10 08:34:21 +0200

    Create

---
+++ project1_folder/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "georgiman"
+
+    workspaces {
+      name = "gh-actions-demo2"
+    }
+  }
+}
+
+resource "random_pet" "name333" {
+  length    = "3"
+  separator = "-"
+}
